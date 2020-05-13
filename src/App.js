bf19a1b23057c92837974b70dd4a05e7ad9a7786
ushanfernando <ushansankalpafernando@gmail.com>
2020-05-13 14:26:45 +0530

    Initial Build

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import  React , {Component } from 'react';
 import './App.css';
+import Wrapper from './Hoc/Wrapper'
+import NavBar from './Components/NavBar/NavBar'
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <Wrapper>
+        <NavBar />
+
+        <h1>This is Home Page</h1>
+
+        
+        </Wrapper>
+    )
+  }
 }
 
 export default App;
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+
+        <nav class="navbar navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">Navbar</a>
+      </nav>
+    )
+}
+
+export default NavBar
