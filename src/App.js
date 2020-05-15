4b24727ad504f33798dece62174aaf911a22d840
Pavan <pavanpabs@gmail.com>
2020-05-15 14:27:25 +0530

    Comment msgs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import './App.css';
 import Wrapper from './Hoc/Wrapper'
 import NavBar from './Components/NavBar/NavBar'
 import SingleProduct from './Containers/SingleProduct'
+import Cart from './Containers/Cart'
 import AdminPage from './Containers/AdminPage/AdminPage'
 import Home from './Containers/Home/Home'
 import Login from './Containers/Login'
 import 'font-awesome/css/font-awesome.min.css';
 import Register from './Containers/Register';
 class App extends Component {
-
+  cnt=0;
   render() {
     return (
       <Wrapper>
+        
         <NavBar />
+        
         <Router>
           <Switch>
             <Route path="/sp">
               <SingleProduct/>
+            </Route >
+            <Route path="/cart">
+              <Cart/>
             </Route >
             <Route path="/login">
               <Login />
