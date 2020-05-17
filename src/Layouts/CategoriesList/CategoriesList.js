17728660c19e632dfa87b446f88724782c5f44e2
ushanfernando <ushansankalpafernando@gmail.com>
2020-05-17 23:28:54 +0530

    fixed bug

--- src/Layouts/CategoriesList/CategoriesList.js
+++ src/Layouts/CategoriesList/CategoriesList.js
@@ -1,27 +1,34 @@
-import React from 'react'
-import Categoty from '../../Components/Category/Category'
-    
-const CategoriesList = (props) => {
-    let categoryRender = null;
-    if (props.categories != null) {
-        categoryRender = props.categories.map((category,index) => {
-            return <Categoty id={index+1} name={category.name} key={category._id}/>
-        })
-    }
-    return (
-        <table className="table overflow-auto" >
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th colspan="3" scope="col">Name</th>
-                </tr>
-            </thead>
-            <tbody>
-                
-                {categoryRender}
-            </tbody>
-        </table>
-    )
-}
+import React from "react";
+import Category from "../../Components/Category/Category";
 
-export default CategoriesList
+const CategoriesList = (props) => {
+  let categoryRender = null;
+  if (props.categories != null) {
+    categoryRender = props.categories.map((category, index) => {
+      return (
+        <Category
+          id={index + 1}
+          name={category.name}
+          key={category._id}
+          onDelete={() => props.onDelete(category._id)}
+          onUpdate={() => props.onUpdate(category)}
+        />
+      );
+    });
+  }
+  return (
+    <table className="table overflow-auto">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th colspan="3" scope="col">
+            Name
+          </th>
+        </tr>
+      </thead>
+      <tbody>{categoryRender}</tbody>
+    </table>
+  );
+};
+
+export default CategoriesList;
