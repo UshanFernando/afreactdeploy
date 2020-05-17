b22c2d6f66374f60a3577f81630be8b3e69e600d
ushanfernando <ushansankalpafernando@gmail.com>
2020-05-17 23:28:38 +0530

    ADDED update and delete for categories

--- src/Components/Category/Category.js
+++ src/Components/Category/Category.js
@@ -6,11 +6,12 @@ function Category(props) {
         <tr>
             <th scope="row">{props.id}</th>
             <td>{props.name}</td>
-            <td><button className="btn btn-primary btn-sm">
+            <td><button className="btn btn-primary btn-sm" onClick={props.onUpdate}>
                 <i className="fa fa-pencil" aria-hidden="true"></i>
                 <span> Edit</span></button></td>
-            <td><button className="btn btn-danger btn-sm">
-                <i className="fa fa-trash" aria-hidden="true"></i>
+            <td><button className="btn btn-danger btn-sm" onClick={props.onDelete}>
+                <i className="fa fa-trash" aria-hidden="true"
+                ></i>
                 <span> Delete</span>
             </button></td>
         </tr>
